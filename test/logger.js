409aa907a0083c80e9756aff366bcbccdf3ada5f
Jay Morrow <j.morrow@wearetuple.co>
2015-11-25 13:13:14 -0600

    Last try

--- test/logger.js
+++ test/logger.js
@@ -3,24 +3,14 @@
 process.env.NODE_ENV = '';
 
 var path = require('path');
-
-var cachePath = path.resolve(__dirname, '..', 'index.js');
-console.log(cachePath);
-if (process.env.CI_ENV === 'travis') {
-    cachePath = '/home/travis/build/jaymorrow/aws-context-logger/index.js';
-}
-delete require.cache[cachePath];
-
 var assert = require('assert');
-var logger = require('../index');
 var listener = require('../test-helpers/listener');
+
+delete require.cache[path.resolve(__dirname, '..', 'index.js')];
+var logger = require('../index');
 
 describe('Using logger', function() {
     var ctx = logger();
-    it('should fail', function (done) {
-        assert.fail(1, 1);
-        done();
-    });
 
     it('should log an error', function (done) {
         var expectedType = 'error';
